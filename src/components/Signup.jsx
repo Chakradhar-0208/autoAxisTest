ae73a04041f3a02b007e998e7d1c2340f2e15923
Chakradhar Kommoju <119922493+Chakradhar-0208@users.noreply.github.com>
2025-07-25 09:22:03 +0530

    Add files via upload

---
+++ src/components/Signup.jsx
@@ -0,0 +1,169 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ModeToggle } from "./mode-toggle";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = "Signup | Authenticator App";
+  }, []);
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const res = await fetch(`${API_URL}/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      const msg = document.getElementById("msg");
+
+      if (res.status === 200) {
+        msg.innerText = "Redirecting to Login Page...";
+        setIsLoading(false);
+        navigate("/login");
+      } else {
+        msg.innerText = data.message;
+      }
+    } catch (err) {
+      console.error("SignUp Error", err);
+      const msg = document.getElementById("msg");
+      msg.innerText = "Something went wrong.";
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    // <div className="flex  dark:bg-[#050505] flex-col justify-center items-center h-[100dvh] min-h-[500px]">
+    <div
+      className="flex flex-col justify-center items-center h-[100dvh] relative"
+      style={{ overflow: "hidden" }}
+    >
+      <div className="absolute top-0 left-0 w-full h-[100dvh] -z-10">
+        <iframe
+          src="https://gentle-priority-829072.framer.app/"
+          style={{
+          opacity:0.8   ,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "130vh", // Push it far down
+            border: "none",
+            zIndex: -1, // Ensure it's behind the form
+          }}
+          allow="fullscreen"
+        ></iframe>
+      </div>
+      {/* <div className="absolute top-6 right-8">
+        <ModeToggle />
+      </div> */}
+      <Card className="w-[90dvw] bg-black/25 min-w-[20rem] sm:w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Create your account</CardTitle>
+          <CardDescription>Fill in the form below to sign up.</CardDescription>
+          <CardAction>
+            <Link to="/login">
+              <Button variant="link" className="px-0">
+                Log in
+              </Button>
+            </Link>
+          </CardAction>
+        </CardHeader>
+
+        <form onSubmit={handleSubmit}>
+          <CardContent className="flex flex-col gap-5">
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                placeholder="John Doe"
+                id="name"
+                name="name"
+                type="text"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                placeholder="john@test.com"
+                id="email"
+                name="email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                placeholder="john"
+                id="password"
+                name="password"
+                type="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </CardContent>
+
+          <CardFooter className="flex flex-col gap-2 mt-4">
+            <p
+              id="msg"
+              className="text-center text-sm text-red-600 min-h-[1.25rem]"
+            ></p>
+            <Button
+              type="submit"
+              disabled={isLoading}
+              className="w-full bg-white/80"
+            >
+              {isLoading ? "Please wait..." : "Sign Up"}
+            </Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default Signup;
